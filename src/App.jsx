6bfbbeb4dcae1c1b2355ec4c35c878ee35a6e573
Calet Devia <calebtius@gmail.com>
2025-08-16 12:21:25 -0500

    children - exe 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import Box from "./components/Box.jsx";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Box>
+        <button className="cursor-pointer">Click me</button>
+      </Box>
+
+      <Box>
+        <p>Hello word</p>
+      </Box>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Box.jsx
@@ -0,0 +1,9 @@
+const Box = ({ children }) => {
+  return (
+    <>
+      <div className="border-1 border-red-400 w-fit p-2">{children}</div>
+    </>
+  );
+};
+
+export default Box;
