de8ae4c4315ab623ccf28efdafbdbff89c1deaec
Calet Devia <calebtius@gmail.com>
2025-08-17 06:39:50 -0500

    children - exe 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import Card from "./components/Card.jsx";
+import Layout from "./components/Layout.jsx";
 
 function App() {
   return (
     <>
-      <Card title={"Title of the card"}>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui ad saepe
-          asperiores dolorum necessitatibus debitis praesentium reiciendis
-          harum, veritatis, dolor obcaecati tempore quia molestias dicta dolores
-          minus ducimus cumque similique.
-        </p>
-      </Card>
-      <br />
-
-      <Card children={"Other description"} title={"Other title"} />
+      <Layout>
+        <header>Header</header>
+        <main>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente,
+          atque numquam repellendus voluptate harum velit delectus? Modi
+          incidunt non quibusdam, accusamus eaque laboriosam error voluptatum
+          deleniti? Explicabo architecto eos harum!
+        </main>
+        <footer>Footer</footer>
+      </Layout>
     </>
   );
 }
---
+++ src/components/Layout.jsx
@@ -0,0 +1,6 @@
+const Layout = ({ children }) => {
+  return <>
+  <h1>layout</h1>
+  {children}</>;
+};
+export default Layout;
