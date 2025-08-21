3e51129feb75d0fbf167d5394e74dfa8fd9e86fc
Calet Devia <calebtius@gmail.com>
2025-08-21 06:32:46 -0500

    children, contextAPI - exe 8

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import ThemeProvider from "./components/ThemeProvider.jsx";
 
 import ThemeButton from "./components/ThemeButton.jsx";
 
+import Box from "./components/Box.jsx";
+
 function App() {
   return (
     <>
       <ThemeProvider>
         <ThemeButton />
-  
+        <Box>
+          <h1>hola</h1>
+        </Box>
       </ThemeProvider>
     </>
   );
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,7 +1,19 @@
+import { useContext } from "react";
+import { ThemeContext } from "./ThemeProvider.jsx";
+
 const Box = ({ children, prove }) => {
+  const { theme } = useContext(ThemeContext);
   return (
     <>
-      <div className="border-1 border-red-400 w-fit p-2">{children}</div>
+      <div
+        className={`border-1 w-20 h-20 p-2 ${
+          theme === "light"
+            ? "border-red-400 bg-amber-200 text-white"
+            : "border-neutral-400 bg-gray-200 text-red-500 text-2xl"
+        }`}
+      >
+        {children}
+      </div>
       {prove}
     </>
   );
